1313bceb66649f15bd47ff3eb9a218372d700939
ismail bahya <ismailbahya20@gmail.com>
2023-07-18 14:54:16 -0700

    done

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,11 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,11 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
